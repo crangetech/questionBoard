366ff7365edc6c5966899e3afa07358ff1c1807b
Christian Rangel <ChristianRangel@Christians-MacBook-Air.local>
2022-11-25 19:37:55 -0800

    created routes for all pages needed

--- server.js
+++ server.js
@@ -1,81 +1,68 @@
 const path = require("path");
-const userRoutes=require('./controlers/api/user-routes.js');
+//const userRoutes=require('./controlers/api/user-routes.js');
 const express = require("express");
 const session = require("express-session");
 const app = express();
 const PORT = process.env.PORT || 3001;
-const sequelize = require("./config/connection");
+//const sequelize = require("./config/connection");
 
 
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const sess = {
-  secret: "Super secret secret",
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// const sess = {
+//   secret: "Super secret secret",
+//   cookie: {},
+//   resave: false,
+//   saveUninitialized: true,
+//   store: new SequelizeStore({
+//     db: sequelize
+//   })
+// };
 app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/api",userRoutes);
-
+//this will be slide 2
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html/'))
 })
+//this will be slide 4
 app.get('/dashboard', (req, res) => {
-    res.sendFile(path.join(__dirname, '/dashboard.html/'))
+    res.sendFile(path.join(__dirname, './public/dashboard.html/'))
 })
-app.get('/entry', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/entry.html/'))
+//This will be the page for slide 5
+app.get('/ask', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/ask.html/'))
 })
-app.get('/signup', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/signup.html/'))
-})
+//not a slide yet
 app.get('/support', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/support.html/'))
 })
+//this will be slide 6
 app.get('/terms-conditions', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/terms-conditions.html/'))
 })
+//this will be slide 3
 app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/register.html/'))
 })
-app.get('/viewAllEntries', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/viewAllEntries.html/'))
-})
+
+//this will be slide 7
 app.get('/privacy-policy', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/privacy-policy.html/'))
 })
+// not a slide yet
 app.get('/loggedOut', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/loggedOut.html/'))
 })
-app.get('/signup', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/signup.html/'))
-})
+
+//not a slide yet
 app.get('/forgot-password', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/forgot-password.html/'))
 })
 
 
-// app.get('/viewAllEntries', (req, res) => {
-//     res.sendFile(path.join(__dirname, '/viewAllEntries.html/'))
-// })
-// app.use('/api',indexRouter);
-//
-//
-// app.use((err,req,res,next)=>{
-//     err.statusCode=err.statusCode || 500;
-//     err.message=err.message || "Internal Server Error";
-//     res.status(err.statusCode).json({
-//         message: err.message,
-//     });
-//
-//
-//
+
 app.listen(PORT,()=>{
 console.log('App listening on port '+PORT);
 sequelize.sync({force:false});
