800a43735b91ae6432b60022fb04f7cc61504d50
danpoggetti <danpoggetti@gmail.com>
2022-12-02 16:35:01 -0800

    added questions-page.js file

---
+++ questionboard/client/src/pages/questions-page.js
@@ -0,0 +1,19 @@
+import '.App.css';
+import Header from './components/Header-comp.js'
+import Nav from '../components/Nav-comp'
+import Footer from './components/Footer-comp.js'
+import Questions from '../components/questions-comp'
+
+function Questions() {
+    return(
+        <div className="">
+            <div>
+            <Header />
+            <Nav />
+            <Questions />
+            <Footer />
+            </div>
+        </div>
+    );
+}
+export default Questions;
