9614a83d7be477b9c12c1e414f422386ddf38e18
danpoggetti <danpoggetti@gmail.com>
2022-12-02 16:39:09 -0800

    updated file

--- questionboard/client/src/pages/privacy-page.js
+++ questionboard/client/src/pages/privacy-page.js
@@ -1,5 +1,6 @@
 import '.App.css';
 import Header from './components/Header-comp.js'
+import Nav from '../components/Nav-comp'
 import Footer from './components/Footer-comp.js'
 import Privacy from '../components/policy-comp'
 
@@ -8,6 +9,7 @@ function Privacy() {
         <div className="">
             <div>
             <Header />
+            <Nav />
             <Privacy />
             <Footer />
             </div>
