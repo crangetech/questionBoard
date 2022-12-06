d83e85e4be1bb141e7e296abf1fafb524cde6fea
Codeword <saldatejared@gmail.com>
2022-12-06 13:17:42 -0800

    fixed

--- questionboard/client/src/pages/terms-page.js
+++ questionboard/client/src/pages/terms-page.js
@@ -6,15 +6,6 @@ import Footer from '../components/Footer-comp.js'
 import Terms from '../components/Terms-comp'
 
 function TermsPage() {
-=======
-import '.App.css';
-import Header from './components/Header-comp.js'
-import Nav from '../components/Nav-comp'
-import Footer from './components/Footer-comp.js'
-import Terms from '../components/Term-comp'
-
-function Terms() {
->>>>>>> cfceb140dc3c1fc5fb6a182334f4c86d8afc6658
     return(
         <div className="">
             <div>
@@ -26,9 +17,5 @@ function Terms() {
         </div>
     );
 }
-<<<<<<< HEAD
 export default TermsPage;
-=======
-export default Terms;
->>>>>>> cfceb140dc3c1fc5fb6a182334f4c86d8afc6658
 
