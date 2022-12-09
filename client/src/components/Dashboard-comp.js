415ee330297e7bad69653060680ed18aa7634736
Diego <diegopena2022@gmail.com>
2022-12-08 19:02:36 -0800

    updated questions

--- client/src/components/Dashboard-comp.js
+++ client/src/components/Dashboard-comp.js
@@ -3,7 +3,7 @@ import { useMutation } from '@apollo/client';
 import { ADD_QUESTION } from '../utils/mutations';
 
 
-function DashboardComp() {
+function DashboardComp(props) {
     const [formState, setFormState] = useState({ question: '' });
     const [addQuestion] = useMutation(ADD_QUESTION);
     // const endpoint =
@@ -35,54 +35,27 @@ function DashboardComp() {
 
     return (
         <main>
-<<<<<<< HEAD
-    <div>
-        <div className>
-            <h2 className>
-                Welcome to your Question Dashboard!
-            </h2>
-            <p className>
-                Have a question? Ask the community.
-            </p>
-            <form onSubmit={handleFormSubmit}>
-                <label htmlFor="title">What is your Question?</label>
-                <input id="question" name="question" value={formState.question} onChange={handleChange} type="question" /><br />
-                <button type="submit">Post Question</button><br /><br />
-            </form>
-        </div>
-        <div className>
-            <h3 className>
-                Previously asked questions:
-            </h3>
-            {/* Table of previous questions can go here */}
-        </div>
-    </div>
-=======
-        <div className="containers w3-display-left">
-            <div className="row">
-                <div className="col-md-12">
-                <h2 >
-                    Welcome to your Question Dashboard!
-                </h2>
-                <p className="">
-                    Have a question? Ask the community.
-                </p>
-                <form id>
-                    <label htmlFor="title">What is your Question?</label>
-                    <input type="text" name id className /><br />
-                    <button>Post Question</button><br /><br />
-                </form>
-                
-                    <div className>
-                <h3 className>
-                    Previously asked questions:
-                </h3>
-                {/* Table of previous questions can go here */}
-                    </div>
+            <div>
+                <div className>
+                    <h2 className>
+                        Welcome to your Question Dashboard!
+                    </h2>
+                    <p className>
+                        Have a question? Ask the community.
+                    </p>
+                    <form onSubmit={handleFormSubmit}>
+                        <label htmlFor="title">What is your Question?</label>
+                        <input id="question" name="questionText" value={formState.questionText} onChange={handleChange} type="question" /><br />
+                        <button type="submit">Post Question</button><br /><br />
+                    </form>
+                </div>
+                <div className>
+                    <h3 className>
+                        Previously asked questions:
+                    </h3>
+                    {/* Table of previous questions can go here */}
                 </div>
             </div>
-        </div>
->>>>>>> b7d4806c5cf0fe666563e870c468cfcff61bc567
         </main >
 
 
